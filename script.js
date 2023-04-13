2f3793b1abcb0c8240ee2364c6996722ecd8f75f
Benjamin Hughes <behu@kea.dk>
2023-04-13 10:43:21 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const checkboxContainer = document.getElementById('checkbox-container');
+
+function createCheckboxes() {
+    for (let i = 1; i <= 30; i++) {
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('checkbox-wrapper');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = `day-${i}`;
+
+        const label = document.createElement('label');
+        label.htmlFor = `day-${i}`;
+        label.textContent = `Day ${i}`;
+
+        wrapper.appendChild(checkbox);
+        wrapper.appendChild(label);
+        checkboxContainer.appendChild(wrapper);
+
+        checkbox.addEventListener('change', () => {
+            localStorage.setItem(`day-${i}`, checkbox.checked);
+        });
+
+        const savedValue = localStorage.getItem(`day-${i}`);
+        if (savedValue !== null) {
+            checkbox.checked = JSON.parse(savedValue);
+        }
+    }
+}
+
+createCheckboxes();
